f1def91ba3bcbe5d3c224e0f88064e82479017ff
DianaTel <130363302+DianaTel@users.noreply.github.com>
2023-09-03 14:06:35 +0000

    Deploying to gh-pages from @ DianaTel/goit-js-hw-09@3fcc4b730b202cf8d9ca820a4274dd8b6a9d589f 🚀

---
+++ 01-color-switcher.f56fe13c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=0;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n||(n=setInterval(a,1e3),t.disabled=!0)})),e.addEventListener("click",(function(){clearInterval(n),n=0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f56fe13c.js.map
